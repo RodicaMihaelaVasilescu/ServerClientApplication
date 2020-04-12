b7feb4295fd6ca08985f7dfb8ce9b4d617079fb9
Rodica Vasilescu <vasilescu.rodica@yahoo.com>
2020-04-12 17:03:52 +0300

    add client, server

---
+++ Socket/Client/Client.cs
@@ -0,0 +1,108 @@
+﻿// A C# program for Client 
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+
+  class Client
+  {
+
+    // Main Method 
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Client");
+      while (true)
+      {
+        ExecuteClient();
+      }
+    }
+
+    // ExecuteClient() Method 
+    static void ExecuteClient()
+    {
+
+      try
+      {
+
+        // Establish the remote endpoint  
+        // for the socket. This example  
+        // uses port 11111 on the local  
+        // computer. 
+        IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());
+        IPAddress ipAddr = ipHost.AddressList[0];
+        IPEndPoint localEndPoint = new IPEndPoint(ipAddr, 11111);
+
+        // Creation TCP/IP Socket using  
+        // Socket Class Costructor 
+        Socket sender = new Socket(ipAddr.AddressFamily,
+                   SocketType.Stream, ProtocolType.Tcp);
+
+        try
+        {
+
+          // Connect Socket to the remote  
+          // endpoint using method Connect() 
+          sender.Connect(localEndPoint);
+
+          // We print EndPoint information  
+          // that we are connected 
+          //Console.WriteLine("Socket connected to -> {0} ",
+          //              sender.RemoteEndPoint.ToString());
+
+          // Creation of messagge that 
+          // we will send to Server 
+          string clientIP = ((IPEndPoint)(sender.RemoteEndPoint)).Address.ToString();
+          Console.Write("Write from client: ");
+          string s = Console.ReadLine();
+          byte[] messageSent = Encoding.ASCII.GetBytes(DateTime.Now+": "+ s + "<EOF>");
+          int byteSent = sender.Send(messageSent);
+
+          // Data buffer 
+          byte[] messageReceived = new byte[1024];
+
+          // We receive the messagge using  
+          // the method Receive(). This  
+          // method returns number of bytes 
+          // received, that we'll use to  
+          //// convert them to string 
+          int byteRecv = sender.Receive(messageReceived);
+          Console.WriteLine("Message from Server: {0}",
+                Encoding.ASCII.GetString(messageReceived,
+                                           0, byteRecv));
+
+          // Close Socket using  
+          // the method Close() 
+          sender.Shutdown(SocketShutdown.Both);
+          sender.Close();
+        }
+
+        // Manage of Socket's Exceptions 
+        catch (ArgumentNullException ane)
+        {
+
+          Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
+        }
+
+        catch (SocketException se)
+        {
+
+          Console.WriteLine("SocketException : {0}", se.ToString());
+        }
+
+        catch (Exception e)
+        {
+          Console.WriteLine("Unexpected exception : {0}", e.ToString());
+        }
+      }
+
+      catch (Exception e)
+      {
+
+        Console.WriteLine(e.ToString());
+      }
+    }
+  }
+}
